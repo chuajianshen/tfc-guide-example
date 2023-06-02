ccbb3b15722dfd370186ba87a6d790dc0865c7be
chuajianshen <77434180+chuajianshen@users.noreply.github.com>
2023-06-02 19:29:25 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ resource "aws_instance" "ubuntu" {
     Name = var.instance_name
   }
 }
-resource "aws_s3_bucket" "shenbucket622023" {
+resource "shenbucket622023" "shenbucket622023" {
   bucket = "shen"
 
   tags = {
